68fa75536288854294b1b8eab21b98c495e31cd6
Intma1n <forestraccoon999@gmail.com>
2021-10-07 10:16:00 +0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,91 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource, request
+from database import GetUser
+
+app = Flask(__name__)
+api = Api(app)
+
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+
+
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+class Users(Resource):
+    def get(self):
+        my_user = GetUser()
+        res = my_user.get_all_user()
+        return res, 200
+
+
+class User(Resource):
+    def get(self, user_id):
+        my_user = GetUser()
+        res = my_user.get_user_by_id(user_id)
+        return {'user': res}
+
+    def delete(self, user_id):
+        pass
+
+    def put(self, user_id):
+        pass
+
+    def post(self, user_id):
+        pass
+
+
+
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return 'sss', 200
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
+api.add_resource(Users, '/users')
+api.add_resource(User, '/user/<user_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,248 @@
+import psycopg2
+
+
+class Base:
+    def __init__(self):
+        self.conn = psycopg2.connect(dbname='historical_reconstruction',
+                                     user='postgres',
+                                     password='144ntthbssioeay',
+                                     host='localhost')
+        self.cursor = self.conn.cursor()
+
+
+class GetUser(Base):
+
+    def get_all_user(self):
+        """
+        GET
+        :return:
+        """
+        self.cursor.execute("SELECT * FROM users")
+        res = self.cursor.fetchall()
+        return list(res)
+
+    def get_user_by_id(self, id):
+        """
+        GET
+        :param id:
+        :return:
+        """
+        self.cursor.execute(f"SELECT * FROM users where id = {id}")
+        res = self.cursor.fetchall()
+        return list(res)
+
+    def delete_user_by_id(self, id):
+        """
+        DELETE
+        :param id:
+        :return:
+        """
+        try:
+            self.cursor.execute(f"delete from users where id = {id}")
+            return True
+        except Exception:
+            return False
+
+    def create_new_user(self, name, password, surname, email, type_):
+        """
+        POST
+        :param name:
+        :param password:
+        :param surname:
+        :param email:
+        :param type_:
+        :return:
+        """
+        try:
+            sql = f"insert into users(name, password, surname, email, type_) values ({name}, {password}, '{surname}', '{email}','{type_}');"
+            self.cursor.execute(sql)
+            return True
+        except Exception:
+            return False
+
+    def update_user(self, id, args: dict):
+        """
+        PUT - FIX BUG
+        :param id:
+        :param args:
+        :return:
+        """
+        for arg in args:
+            if arg == 'name':
+                name = args['name']
+                sql = f"update users set name = '{name}' where id = {id}"
+                self.cursor.execute(sql)
+            elif arg == 'password':
+                password = args['password']
+                sql = f"update users set password = '{password}' where id = {id}"
+                self.cursor.execute(sql)
+            elif arg == 'surname':
+                surname = args['surname']
+                sql = f"update users set email = '{surname}' where id = {id}"
+                self.cursor.execute(sql)
+            elif arg == 'email':
+                email = args['email']
+                sql = f"update users set email = '{email}' where id = {id}"
+                self.cursor.execute(sql)
+            elif arg == 'type_' and (args['type_'] =='organizer' or args['type_'] == 'reenactor' or args['type_']=='default_user'):
+                type_ = args['type_']
+                sql = f"update users set type_ = '{type_}' where id = {id}"
+                self.cursor.execute(sql)
+
+
+class GetBudget(Base):
+    def get_all_budget(self):
+        """*
+        GET
+        :return:
+        """
+        sql = "select * from budget_log"
+        self.cursor.execute(sql)
+        res = self.cursor.fetchall()
+        return list(res)
+
+    def get_budget_by_id(self, id):
+        """
+        GET
+        :param id:
+        :return:
+        """
+        self.cursor.execute(f"SELECT * FROM budget_log where id = {id}")
+        res = self.cursor.fetchall()
+        return list(res)
+
+    def create_new_budget(self, amount, type_budget):
+        """
+        POST
+        :param name:
+        :param password:
+        :param surname:
+        :param email:
+        :param type_:
+        :return:
+        """
+        try:
+            sql = f"insert into budget_log(amount, type_budget) values ('{amount}','{type_budget}');"
+            self.cursor.execute(sql)
+            return True
+        except Exception:
+            return False
+
+    def update_budget(self, id, args: dict):
+        """
+        PUT - FIX BUG
+        :param id:
+        :param args:
+        :return:
+        """
+        for arg in args:
+            if arg == 'amount':
+                amount = args['amount']
+                sql = f"update budget_log set amount = '{amount}' where id = {id}"
+                self.cursor.execute(sql)
+            elif arg == 'type_budget' and (args['type_budget'] == 'received' or args['type_budget'] == 'spent'):
+                type_budget = args['type_budget']
+                sql = f"update budget_log set type_budget = '{type_budget}' where id = {id}"
+                self.cursor.execute(sql)
+
+    def delete_budget_by_id(self, id):
+        """
+        DELETE
+        :param id:
+        :return:
+        """
+        try:
+            self.cursor.execute(f"delete from budget_log where id = {id}")
+            return True
+        except Exception:
+            return False
+
+
+class GetEquipment(Base):
+    def get_all_equipment(self):
+        """
+        GET
+        :return:
+        """
+        sql = "select * from equipment"
+        self.cursor.execute(sql)
+        res = self.cursor.fetchall()
+        return list(res)
+
+    def get_equipment_by_id(self, id):
+        """
+        GET
+        :param id:
+        :return:
+        """
+        self.cursor.execute(f"SELECT * FROM equipment where id = {id}")
+        res = self.cursor.fetchall()
+        return list(res)
+
+    def create_new_equipment(self, name, access_type, is_available, type_equip):
+        """
+        POST
+        :param name:
+        :param password:
+        :param surname:
+        :param email:
+        :param type_:
+        :return:
+        """
+        try:
+            sql = f"insert into equipment(name, access_type, is_available, type_equip) values " \
+                  f"('{name}', '{access_type}', '{is_available}', '{type_equip}');"
+            self.cursor.execute(sql)
+            return True
+        except Exception:
+            return False
+
+    def update_equipment(self, id, args: dict):
+        """
+        PUT - FIX BUG
+        :param id:
+        :param args:
+        :return:
+        """
+        for arg in args:
+            if arg == 'name':
+                name = args['name']
+                sql = f"update equipment set name = '{name}' where id = {id}"
+                self.cursor.execute(sql)
+            elif arg == 'access_type' and (args['access_type'] == 'organizer' or args['access_type'] == 'reenactor', args['default_user'] == 'default_user'):
+                access_type = args['access_type']
+                sql = f"update equipment set access_type = '{access_type}' where id = {id}"
+                self.cursor.execute(sql)
+            elif arg == 'is_available':
+                is_available = args['is_available']
+                sql = f"update equipment set is_available = '{is_available}' where id = {id}"
+                self.cursor.execute(sql)
+            elif arg == 'type_equip' and (args['type_equip'] == 'special' or args['type_equip'] == 'common'):
+                type_equip = args['type_equip']
+                sql = f"update equipment set type_equip = '{type_equip}' where id = {id}"
+                self.cursor.execute(sql)
+
+    def delete_equipment(self, id):
+        """
+        DELETE
+        :param id:
+        :return:
+        """
+        try:
+            self.cursor.execute(f"delete from budget_log where id = {id}")
+            return True
+        except Exception:
+            return False
+
+
+
+def main():
+        my_user = GetUser()
+        dict_change = {'email': 'asdasdasd@gmail.com', 'name': 'ash'}
+        #my_user.update_new_user(2, dict_change)
+        res = my_user.get_user_by_id(2)
+        print(res)
+
+if __name__ == '__main__':
+    main()
+
